dd649b3a4c6a3bd97ae4cb23c7543d234a82210d
Hobert Bush <hobertbush@gmail.com>
2020-09-03 18:00:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
